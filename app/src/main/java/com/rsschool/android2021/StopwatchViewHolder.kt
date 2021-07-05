65301d24cf763c6a856e753b5d5de7ebd3badae3
bumis95 <bumis95@gmail.com>
2021-07-05 13:41:54 +0300

    Add timer

--- app/src/main/java/com/rsschool/android2021/StopwatchViewHolder.kt
+++ app/src/main/java/com/rsschool/android2021/StopwatchViewHolder.kt
@@ -1,14 +1,42 @@
 package com.rsschool.android2021
 
+import android.os.CountDownTimer
 import androidx.recyclerview.widget.RecyclerView
 import com.rsschool.android2021.databinding.StopwatchItemBinding
 
 class StopwatchViewHolder(
     private val binding: StopwatchItemBinding
-): RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private var timer: CountDownTimer? = null
 
     fun bind(stopwatch: Stopwatch) {
         binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+
+        if (stopwatch.isStarted) {
+            startTimer(stopwatch)
+        }
+    }
+
+    private fun startTimer(stopwatch: Stopwatch) {
+        timer?.cancel()
+        timer = getCountDownTimer(stopwatch)
+        timer?.start()
+    }
+
+    private fun getCountDownTimer(stopwatch: Stopwatch): CountDownTimer {
+        return object : CountDownTimer(PERIOD, UNIT_TEN_MS) {
+            val interval = UNIT_TEN_MS
+
+            override fun onTick(millisUntilFinished: Long) {
+                stopwatch.currentMs += interval
+                binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+            }
+
+            override fun onFinish() {
+                binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+            }
+        }
     }
 
     private fun Long.displayTime(): String {
@@ -34,5 +62,7 @@ class StopwatchViewHolder(
     private companion object {
 
         private const val START_TIME = "00:00:00:00"
+        private const val UNIT_TEN_MS = 10L
+        private const val PERIOD = 1000L * 60L * 60L * 24L // Day
     }
 }
