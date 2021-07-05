8c34095f70a5651e65ef7fd2d3d62f7144c5b278
bumis95 <bumis95@gmail.com>
2021-07-05 14:56:29 +0300

    Add buttons

--- app/src/main/java/com/rsschool/android2021/MainActivity.kt
+++ app/src/main/java/com/rsschool/android2021/MainActivity.kt
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rsschool.android2021.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StopwatchListener {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val stopwatchAdapter = StopwatchAdapter()
+    private val stopwatchAdapter = StopwatchAdapter(this)
     private val stopwatches = mutableListOf<Stopwatch>()
     private var nextId = 0
 
@@ -25,8 +25,39 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.addNewStopwatchButton.setOnClickListener {
-            stopwatches.add(Stopwatch(nextId++, 0, true))
+            stopwatches.add(Stopwatch(nextId++, 0, false))
             stopwatchAdapter.submitList(stopwatches.toList())
         }
+    }
+
+    override fun start(id: Int) {
+        changeStopwatch(id, null, true)
+    }
+
+    override fun stop(id: Int, currentMs: Long) {
+        changeStopwatch(id, currentMs, false)
+    }
+
+    override fun reset(id: Int) {
+        changeStopwatch(id, 0L, false)
+    }
+
+    override fun delete(id: Int) {
+        stopwatches.remove(stopwatches.find { it.id == id })
+        stopwatchAdapter.submitList(stopwatches.toList())
+    }
+
+    private fun changeStopwatch(id: Int, currentMs: Long?, isStarted: Boolean) {
+        val newTimers = mutableListOf<Stopwatch>()
+        stopwatches.forEach {
+            if (it.id == id) {
+                newTimers.add(Stopwatch(it.id, currentMs ?: it.currentMs, isStarted))
+            } else {
+                newTimers.add(it)
+            }
+        }
+        stopwatchAdapter.submitList(newTimers)
+        stopwatches.clear()
+        stopwatches.addAll(newTimers)
     }
 }
